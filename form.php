c258c390f2211c46a56f3e72a0df71b2ced8c8a8
UNCLEBREAKER <45223997+TitoUncle123@users.noreply.github.com>
2019-02-20 14:48:18 +0800

    REGISTRATION FORM
    FORMS

---
+++ form.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Registration Form";
+
+echo "Name:";
+
+echo "Grade & Section: ";
+
+echo "Birthday: ";
+
+echo "Gender: ";
+
+echo "Email: ";
+
+echo "Password:";
+
+echo "Contact No.: ";
+
+?>
